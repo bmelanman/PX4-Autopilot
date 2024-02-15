fa042a2164269e5b108c409b2b9d036ec85effff
Bryce Melander <brycemelander@gmail.com>
2024-02-15 11:12:06 -0800

    Wrote a new angle calculation function to account for offsets and range limits better

--- src/modules/open_gimbal/output_rc.cpp
+++ src/modules/open_gimbal/output_rc.cpp
@@ -48,19 +48,38 @@ OutputRC::OutputRC(const Parameters &parameters)
 {
 }
 
+// Convert the range [-180, 179.999] to the range [-32768, 32767]
+#define NORM_ANGLE_TO_INT16 ( 182.0416666667f )
+// ( (float)( ( 1 << 16 ) - 1 ) / 360.0f ) = 182.0416666667f
+
 /**
  * @brief Converts a target angle in degrees to a gimbal control value in the range [-1, 1]
- * @param target_angle - The target angle in radians
- * @param range - The range in radians
+ * @param target_angle - The target angle in degrees.
+ * @param angle_offset - The angle offset in degrees.
+ * @param angle_limit - The maximum/minimum angle in degrees. A value of 0 with apply no limit.
  * @return - A gimbal control value in the range [-1, 1]
  */
-float calculate_gimbal_control(float target_angle, float range)
+inline float calculate_gimbal_control(float target_angle, float angle_offset, float angle_limit)
 {
-	// Constrain the target angle to within the range
-	float target_angle_constrained = constrain(target_angle, -range / 2.0f, range / 2.0f);
+	// Normalize the inputs to the range [-32768, 32767]
+	// (i.e. -32768 = -180, 0 = 0, 32767 = 179.999, etc.)
 
-	// Normalize the target angle to the range [-1, 1]
-	return target_angle_constrained / (range / 2.0f);
+	// Need to round to make sure 180 degrees overflows to -180 degrees
+	int16_t target_angle_norm = (int16_t)round(target_angle * NORM_ANGLE_TO_INT16),
+		angle_offset_norm = (int16_t)(angle_offset * NORM_ANGLE_TO_INT16),
+		final_angle = (target_angle_norm + angle_offset_norm);
+
+	// Apply the angle limit if it is greater than 0
+	if (angle_limit > 0) {
+		int16_t angle_limit_norm = (int16_t)(angle_limit * NORM_ANGLE_TO_INT16);
+
+		final_angle = constrain(final_angle,
+					(int16_t)(~(angle_limit_norm) + 1), // 2's complement
+					angle_limit_norm);
+	}
+
+	// Add the offset to the target angle and normalize the result to fit within [-1, 1]
+	return (float)final_angle / 32767.0f;
 }
 
 void OutputRC::update(const ControlData &control_data, bool new_setpoints, uint8_t &gimbal_device_id)
@@ -85,24 +104,56 @@ void OutputRC::update(const ControlData &control_data, bool new_setpoints, uint8
 	gimbal_controls_s gimbal_controls{};
 
 	gimbal_controls.control[gimbal_controls_s::INDEX_ROLL] = calculate_gimbal_control(
-			_angle_outputs[0] + math::radians(_parameters.mnt_off_roll),
-			math::radians(_parameters.mnt_range_roll)
+			math::degrees(_angle_outputs[0]),
+			_parameters.mnt_off_roll,
+			_parameters.mnt_range_roll
 		);
 
 	gimbal_controls.control[gimbal_controls_s::INDEX_PITCH] = calculate_gimbal_control(
-			_angle_outputs[1] + math::radians(_parameters.mnt_off_pitch),
-			math::radians(_parameters.mnt_range_pitch)
+			math::degrees(_angle_outputs[1]),
+			_parameters.mnt_off_pitch,
+			_parameters.mnt_range_pitch
 		);
 
 	gimbal_controls.control[gimbal_controls_s::INDEX_YAW] = calculate_gimbal_control(
-			_angle_outputs[2] + math::radians(_parameters.mnt_off_yaw),
-			math::radians(_parameters.mnt_range_yaw)
+			math::degrees(_angle_outputs[2]),
+			_parameters.mnt_off_yaw,
+			_parameters.mnt_range_yaw
 		);
 
 	gimbal_controls.timestamp = hrt_absolute_time();
 	_gimbal_controls_pub.publish(gimbal_controls);
 
 	_last_update = t;
+
+	//static uint16_t counter = 0;
+	//if (counter++ % 500 == 0) {
+	//	// Save cursor position
+	//	PX4_INFO_RAW("\0337");
+
+	//	// Print the target angles
+	//	PX4_INFO_RAW("\033[1;60H"); // Move to (1, 60)
+	//	PX4_INFO_RAW("Target Angles (deg):");
+	//	PX4_INFO_RAW("\033[2;60H"); // Move to (2, 60)
+	//	PX4_INFO_RAW("Roll:  %8.4f\n", (double)math::degrees(_angle_outputs[0]));
+	//	PX4_INFO_RAW("\033[3;60H"); // Move to (3, 60)
+	//	PX4_INFO_RAW("Pitch: %8.4f\n", (double)math::degrees(_angle_outputs[1]));
+	//	PX4_INFO_RAW("\033[4;60H"); // Move to (4, 60)
+	//	PX4_INFO_RAW("Yaw:   %8.4f\n", (double)math::degrees(_angle_outputs[2]));
+
+	//	// Print the angles after conversion
+	//	PX4_INFO_RAW("\033[6;60H"); // Move to (6, 60)
+	//	PX4_INFO_RAW("Output angles (-180,180):");
+	//	PX4_INFO_RAW("\033[7;60H"); // Move to (7, 60)
+	//	PX4_INFO_RAW("Roll:  %8.4f\n", (double)gimbal_controls.control[gimbal_controls_s::INDEX_ROLL]);
+	//	PX4_INFO_RAW("\033[8;60H"); // Move to (8, 60)
+	//	PX4_INFO_RAW("Pitch: %8.4f\n", (double)gimbal_controls.control[gimbal_controls_s::INDEX_PITCH]);
+	//	PX4_INFO_RAW("\033[9;60H"); // Move to (9, 60)
+	//	PX4_INFO_RAW("Yaw:   %8.4f\n", (double)gimbal_controls.control[gimbal_controls_s::INDEX_YAW]);
+
+	//	// Restore cursor position
+	//	PX4_INFO_RAW("\0338");
+	//}
 }
 
 void OutputRC::print_status() const
