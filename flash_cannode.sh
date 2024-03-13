4bac5d03483cb6aec3cd5213b587a3a6761d6be2
Bryce Melander <brycemelander@gmail.com>
2024-03-13 16:15:08 -0700

    File created

---
+++ flash_cannode.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script is used to flash the cannode firmware to the cannode board
+BOARD_NAME=ark_cannode_default
+
+# Run the build script
+make "${BOARD_NAME}"
+
+# Set the path to the firmware
+ELF_FILE=./build/"${BOARD_NAME}"/"${BOARD_NAME}".elf
+
+# Start a JLinkGDBServer
+JLinkGDBServer \
+	-if SWD \
+	-port 2331 \
+	-speed 4000 \
+	-device STM32F103C8 \
+	-singlerun -nogui &
+
+# Start a GDB session
+arm-none-eabi-gdb "${ELF_FILE}" \
+	-ex "target remote localhost:2331" \
+	-ex "monitor reset" \
+	-ex "load" \
+	-ex "monitor reset" \
+	-ex "detach" \
+	-ex "quit"
+
+# Wait for the JLinkGDBServer to finish
+sleep 1
+
+# Done!
+echo -e "\n\nDone!"
