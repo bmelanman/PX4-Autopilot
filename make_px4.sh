3347ef11e5f628051cf4b420d9b24fdc55055b90
Bryce Melander <brycemelander@gmail.com>
2024-04-19 14:52:38 -0700

    Massively improved bash script

--- make_px4.sh
+++ make_px4.sh
@@ -1,10 +1,137 @@
 #!/bin/bash
 
+# Get the current time
+TIME_START=$(date +%s)
+
+# Colors
+RED="\x1b[31m"
+GREEN="\x1b[32m"
+BLUE="\x1b[34m"
+RESET="\x1b[0m"
+
+# DEFAULT_TARGET="ark_cannode_bootloader"
+DEFAULT_TARGET="ark_cannode_default"
+
+# Check for input arguments
+if [ $# -lt 1 ]; then
+	# No arguments provided, use the default target
+	MAKE_TARGET=${DEFAULT_TARGET}
+else
+	# Use the first argument as the target and remove it from the list
+	MAKE_TARGET=$1
+	shift
+fi
+
+BIN_DIR="./build/${MAKE_TARGET}"
+BIN_FILE="${BIN_DIR}/83*.uavcan.bin"
+OLD_BINS="${BIN_DIR}/old_bins"
+
+# Move any old binary files
+mkdir -p ${OLD_BINS}
+mv ${BIN_FILE} ${OLD_BINS}
+
+LOG_DIR="./build_logs"
+LOG_FILE="${LOG_DIR}/${MAKE_TARGET}-$(date +%Y%m%d-%H%M%S).log"
+
+SD_CARD_DIR="/Volumes/PIXHAWK"
+
+function print_info {
+	printf "\n${BLUE}Info:${RESET} $1\n\n"
+}
+
+function print_error {
+	printf "\n${RED}Error:${RESET} $1\n\n"
+	exit 1
+}
+
+function do_task {
+	# Print the task header
+	echo "#======================================#" | tee -a ${LOG_FILE}
+
+	# Print the info message
+	print_info "$1" | tee -a ${LOG_FILE}
+
+	# Do the task
+	printf "${GREEN}Running \"$2\"...${RESET}\n" | tee -a ${LOG_FILE} # >/dev/null 2>&1
+	script -F -q -e -a ${LOG_FILE} $2
+
+	# Check if the task was successful
+	if [ $? -ne 0 ]; then
+		# Print the error message
+		print_error "$3" | tee -a ${LOG_FILE}
+		exit 1
+	else
+		echo ""
+	fi
+
+	# Print the task footer
+	echo "#======================================#" | tee -a ${LOG_FILE}
+}
+
+# Create the build log directory if it doesn't exist
+if [ ! -d ${BUILD_LOG_DIR} ] && [ ! $(mkdir -p ${BUILD_LOG_DIR}) ]; then
+	print_error "Failed to create build log directory: ${BUILD_LOG_DIR}"
+	exit 1
+fi
+
+print_info "Making target \"${MAKE_TARGET}\" and attempting to upload to loacal hardware..."
+
 # Call ulimit to make sure we have enough file descriptors
 ulimit -S -n 2048
 
-# Run make with the arguments passed to this script
-make $@
+# Make the target
+do_task \
+	"Building target: ${MAKE_TARGET}" \
+	"make ${MAKE_TARGET}" \
+	"Failed to build target: ${MAKE_TARGET}"
+
+# Get the path to the binary file
+BIN_FILE="$(realpath ${BIN_FILE})"
+
+if [ ! -f "${BIN_FILE}" ]; then
+	print_error "Binary file not found: ${BIN_FILE}"
+	exit 1
+fi
+
+# If an ST-LINK is connected, flash the binary file, or check if we can copy it to an SD card
+if [ "$(st-info --descr)" ]; then
+
+	# Flash the binary file
+	do_task \
+		"Flashing ${BIN_FILE}..." \
+		"st-flash write ${BIN_FILE} 0x08010000" \
+		"Failed to flash ${BIN_FILE}"
+
+	# Reset the board
+	do_task \
+		"Resetting the board..." \
+		"st-flash reset" \
+		"Failed to reset the board?"
+
+# Check if an SD card exists
+elif [ -d "${SD_CARD_DIR}" ]; then
+
+	# Copy the binary file to the SD card
+	do_task \
+		"Copying ${BIN_FILE} to the SD card..." \
+		"mv ${BIN_FILE} ${SD_CARD_DIR}" \
+		"Failed to copy ${BIN_FILE} to the SD card"
+
+	diskutil eject ${SD_CARD_DIR}
+
+else
+	# Print a warning message
+	print_info "No ST-LINK or SD card found, binary file is located at:\n\t${GREEN}$(realpath ${BIN_FILE})${RESET}"
+fi
+
+# Get the end time
+TIME_END=$(date +%s)
+
+# Calculate the elapsed time
+ELAPSED_TIME=$(($TIME_END - $TIME_START))
+
+# Print the elapsed time
+print_info "Elapsed time: Took ${ELAPSED_TIME} seconds"
 
 # Done!
-echo -e "\n\nDone!"
+print_info "Done!"
